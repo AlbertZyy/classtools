ded0fdb4676c8b4684e4f26f2b7a4b47feff5421
Hyacine <zhengyy0621@foxmail.com>
2025-10-12 00:51:36 +0800

    use python3.12 typehint style

--- classtools.py
+++ classtools.py
@@ -1,20 +1,26 @@
 
-__all__ = ['immutable_property', 'descriptor', 'variantmethod', 'Signal']
+__all__ = [
+    'immutable_property',
+    'descriptor',
+    'variantmethod',
+    'Signal',
+    'signalmethod'
+]
 
 import types
-from typing import TypeVar, Generic, ParamSpec, overload, Concatenate, Any, Self
+from typing import overload, Concatenate, Protocol, Self, Any
 from collections.abc import Callable
 
-_T = TypeVar('_T')
-_KT = TypeVar('_KT')
-_VT = TypeVar('_VT')
-_P = ParamSpec('_P')
-_R_co = TypeVar('_R_co', covariant=True)
+
+### Types
+
+class SupportsGet[T, R](Protocol):
+    def __get__(self, instance: T, owner: type[T]) -> R: ...
 
 
 ### Attribute
 
-class immutable_property(Generic[_T, _VT]): # Similar to the cached_property in functools.py
+class immutable_property[T, VT]: # Similar to the cached_property in functools.py
     """An immutable property reference.
 
     The __get__ method of this descriptor runs as a default factory at the first
@@ -26,7 +32,7 @@ class immutable_property(Generic[_T, _VT]): # Similar to the cached_property in 
     objects can still be mutable."""
     __slots__ = ('__name__', '_default', '_default_factory')
 
-    def __init__(self, fnew: Callable[[_T], _VT] | None = None) -> None:
+    def __init__(self, fnew: Callable[[T], VT] | None = None) -> None:
         self.__name__ = None
         self._default_factory = fnew
 
@@ -42,7 +48,7 @@ class immutable_property(Generic[_T, _VT]): # Similar to the cached_property in 
     @overload
     def __get__(self, obj: None, objtype: type, /) -> Self: ...
     @overload
-    def __get__(self, obj: _T, objtype: type[_T], /) -> _VT: ...
+    def __get__(self, obj: T, objtype: type[T], /) -> VT: ...
     def __get__(self, obj, objtype, /):
         if obj is None:
             return self
@@ -57,16 +63,16 @@ class immutable_property(Generic[_T, _VT]): # Similar to the cached_property in 
             storage[self.__name__] = value
             return value
 
-    def __set__(self, obj: _T, value: _VT, /) -> None:
+    def __set__(self, obj: T, value: VT, /) -> None:
         raise TypeError("can not assign to an immutable property")
 
-    def __delete__(self, obj: _T, /) -> None:
+    def __delete__(self, obj: T, /) -> None:
         storage = self._get_storage(obj)
 
         if self.__name__ in storage:
             del storage[self.__name__]
 
-    def _get_storage(self, obj: _T):
+    def _get_storage(self, obj: T):
         try:
             return obj.__dict__
         except AttributeError as e:
@@ -76,7 +82,7 @@ class immutable_property(Generic[_T, _VT]): # Similar to the cached_property in 
             ) from e
 
 
-def descriptor(factory: Callable[_P, _VT], /) -> Callable[_P, immutable_property[_T, _VT]]:
+def descriptor[**P, VT](factory: Callable[P, VT], /) -> Callable[P, immutable_property[Any, VT]]:
     def wrapper(*args, **kwargs):
         return immutable_property(lambda _: factory(*args, **kwargs))
     return wrapper
@@ -84,18 +90,18 @@ def descriptor(factory: Callable[_P, _VT], /) -> Callable[_P, immutable_property
 
 ### VariantMethod
 
-class _Variant(Generic[_KT, _T, _P, _R_co]):
+class _Variant[KT, T, **P, R]:
     def __init__(
         self,
-        curr_key: _KT,
-        obj: _T,
-        vt_ref: dict[_KT, Callable[Concatenate[_T, _P], _R_co]]
+        curr_key: KT,
+        obj: T,
+        vt_ref: dict[KT, Callable[Concatenate[T, P], R]]
     ):
         self._vt_ref = vt_ref # read-only
         self.curr_key = curr_key
         self.instance = obj
 
-    def __call__(self, *args: _P.args, **kwargs: _P.kwargs) -> _R_co:
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
         try:
             impl = self._vt_ref[self.curr_key]
         except KeyError:
@@ -106,21 +112,21 @@ class _Variant(Generic[_KT, _T, _P, _R_co]):
     def __len__(self) -> int:
         return len(self._vt_ref)
 
-    def __getitem__(self, val: _KT) -> Callable[_P, _R_co]:
+    def __getitem__(self, val: KT) -> Callable[P, R]:
         func = self._vt_ref[val]
         return func.__get__(self.instance)
 
-    def __contains__(self, item: _KT) -> bool:
+    def __contains__(self, item: KT) -> bool:
         return item in self._vt_ref
 
-    def set(self, val: _KT, /) -> None:
+    def set(self, val: KT, /) -> None:
         self.curr_key = val
 
     def mapping(self):
         return types.MappingProxyType(self._vt_ref)
 
 
-class variantmethod(immutable_property[_T, _Variant[_KT, _T, _P, _R_co]], Generic[_KT, _T, _P, _R_co]):
+class variantmethod[KT, T, **P, R](immutable_property[T, _Variant[KT, T, P, R]]):
     """Variant method decorator class for implementing a key-based method
     dispatch mechanism.
 
@@ -130,10 +136,10 @@ class variantmethod(immutable_property[_T, _Variant[_KT, _T, _P, _R_co]], Generi
     to call based on the object's state and the registered key.
 
     Generic parameters:
-        _KT: The type of the key, used to distinguish different method variants
-        _T: The type of the object that owns this property
-        _P: The type of the method parameters
-        _R_co: The return type of the method
+        KT: The type of the key, used to distinguish different method variants
+        T: The type of the object that owns this property
+        **P: The type of the method parameters
+        R: The return type of the method
 
     Examples:
     ```
@@ -158,28 +164,28 @@ class variantmethod(immutable_property[_T, _Variant[_KT, _T, _P, _R_co]], Generi
     my_obj.my_method["c"](42)
     ```
     """
-    virtual_table: dict[_KT, Callable[Concatenate[_T, _P], _R_co]]
+    virtual_table: dict[KT, Callable[Concatenate[T, P], R]]
 
     @overload
-    def __new__(cls, key: _KT, func: Callable[Concatenate[_T, _P], _R_co], /) -> "variantmethod[_KT, _T, _P, _R_co]": ...
+    def __new__(cls, key: KT, func: Callable[Concatenate[T, P], R], /) -> "variantmethod[KT, T, P, R]": ...
     @overload
-    def __new__(cls, key: _KT, /) -> Callable[[Callable[Concatenate[_T, _P], _R_co]], "variantmethod[_KT, _T, _P, _R_co]"]: ...
+    def __new__(cls, key: KT, /) -> Callable[[Callable[Concatenate[T, P], R]], "variantmethod[KT, T, P, R]"]: ...
     def __new__(cls, arg1, arg2=None, /):
         if arg2 is None:
             return lambda func: cls(arg1, func)
         else:
             return super().__new__(cls)
 
-    def __init__(self, key: _KT, func: Callable[Concatenate[_T, _P], _R_co], /):
+    def __init__(self, key: KT, func: Callable[Concatenate[T, P], R], /):
         super().__init__(lambda obj: _Variant(key, obj, self.virtual_table))
         self.virtual_table = {key: func}
         self.__doc__ = func.__doc__
 
-    def __set__(self, obj: _T, value: _VT):
+    def __set__(self, obj, value):
         raise TypeError("can not assign to variant methods")
 
-    def register(self, key: _KT, /):
-        def decorator(func: Callable) -> variantmethod[_KT, _T, _P, _R_co]:
+    def register(self, key: KT, /):
+        def decorator(func: Callable) -> variantmethod[KT, T, P, R]:
             self.virtual_table[key] = func
             return self
         return decorator
@@ -187,16 +193,16 @@ class variantmethod(immutable_property[_T, _Variant[_KT, _T, _P, _R_co]], Generi
 
 ### Signal
 
-class _Emitter(Generic[_VT]):
-    def __init__(self, obj, cb_ref: list[Callable[[_VT], Any]]):
+class _Emitter[VT]:
+    def __init__(self, obj, cb_m: list, cb_f: list):
         self.obj = obj
-        self._cb_ref = cb_ref # read-only
-        self._cb_list = [func.__get__(self.obj, self.obj.__class__) for func in self._cb_ref]
+        self._cb_list = [func.__get__(self.obj, self.obj.__class__) for func in cb_m]
+        self._cb_list.extend(cb_f)
 
     @overload
     def emit(self) -> None: ...
     @overload
-    def emit(self, value: _VT, /) -> None: ...
+    def emit(self, value: VT, /) -> None: ...
     def emit(self, *args) -> None:
         import inspect
 
@@ -204,21 +210,23 @@ class _Emitter(Generic[_VT]):
             raise TypeError("too many arguments for emit")
 
         for func in self._cb_list:
-            if inspect.signature(func).parameters:
-                try:
-                    value = (args[0],)
-                except IndexError:
-                    value = ()
+            try:
+                params = inspect.signature(func).parameters
+            except TypeError:
+                continue
+
+            if params:
+                value = args[:1] # 0 or 1 arg
             else:
                 value = ()
 
             func(*value)
 
-    def connect(self, target: Callable[[_VT], _R_co], /):
+    def connect[R](self, target: Callable[[VT], R], /):
         self._cb_list.append(target)
         return target
 
-    def disconnect(self, target: Callable[[_VT], _R_co], /):
+    def disconnect[R](self, target: Callable[[VT], R], /):
         try:
             self._cb_list.remove(target)
         except ValueError:
@@ -226,23 +234,43 @@ class _Emitter(Generic[_VT]):
         return target
 
 
-class Signal(immutable_property[_T, _Emitter[_VT]]):
-    callbacks: list[Callable[[_VT], Any]]
-
-    def __init__(self, dtype: type[_VT], /):
-        super().__init__(lambda obj: _Emitter(obj, self.callbacks))
-        self.callbacks = []
+class Signal[T, VT](immutable_property[T, _Emitter[VT]]):
+    def __init__(self, dtype: type[VT] | None = None, /):
+        super().__init__(lambda obj: _Emitter(obj, self._cb_m, self._cb_f))
+        self._cb_m = []
+        self._cb_f = []
         self.dtype = dtype
 
-    def __set__(self, obj: _T, value: _VT):
+    def __set__(self, obj: T, value: VT):
         raise TypeError("can not assign to signals")
 
-    def bind(self, func: Callable[[_VT], _R_co], /):
-        if not callable(func) and not hasattr(func, "__get__"):
-            raise TypeError(f"{func!r} is not callable or a descriptor")
-        self.callbacks.append(func)
+    def bindm[R](self, func: SupportsGet[T, Callable[[VT], R]], /):
+        """Binds a descriptor to the signal. The descriptor supports
+        `__get__` returning a callable."""
+        if not hasattr(func, "__get__"):
+            raise TypeError(f"{func!r} is not a descriptor")
+        self._cb_m.append(func)
         return func
 
-    def unbind(self, func: Callable[[_VT], _R_co], /):
-        self.callbacks.remove(func)
+    def unbindm[R](self, func: SupportsGet[T, Callable[[VT], R]], /):
+        self._cb_m.remove(func)
         return func
+
+    def bindf[R](self, func: Callable[[VT], R], /):
+        """A decorator that binds a callable to the signal."""
+        if not callable(func):
+            raise TypeError(f"{func!r} is not callable")
+        self._cb_f.append(func)
+        return func
+
+    def unbindf[R](self, func: Callable[[VT], R], /):
+        self._cb_f.remove(func)
+        return func
+
+
+# Here we use Callable instead of SupportsGet for a better type deduction.
+def signalmethod[T, V](func: Callable[[T, V], Any], /) -> Signal[T, V]:
+    """Create a signal method from a function binded."""
+    s = Signal()
+    s.bindm(func) # This checks if func is a descriptor.
+    return s
